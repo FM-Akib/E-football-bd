5fef467afb6ab20c82d4950a5fd60e13da13f7c7
FM-Akib <muntasirakib08@gmail.com>
2024-01-19 18:44:52 +0600

    calculation added

--- src/components/Signed/Signed.jsx
+++ src/components/Signed/Signed.jsx
@@ -4,13 +4,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faEuroSign } from '@fortawesome/free-solid-svg-icons';
 const Signed = (prop) => {
     const playerCart=prop.playerCart;
-    console.log(playerCart);
+    
+    let totalPrice=0;
+    for(const player of playerCart){
+        totalPrice += player.price;
+    }
     return (
         <div className="SignedContainer">
             <h3>Total Player <FontAwesomeIcon icon={faArrowRight} style={{color: "#ffffff"}} />  {playerCart.length}</h3>
-            <h3>Total Price <FontAwesomeIcon icon={faArrowRight} style={{color: "#ffffff"}} /> </h3>
+            <h3>Total Price <FontAwesomeIcon icon={faArrowRight} style={{color: "#ffffff"}} />  {totalPrice} <FontAwesomeIcon icon={faEuroSign} style={{color: "#ffffff",}} /></h3>
            
             <button className="btn-clear btn">Clear Cart  <FontAwesomeIcon icon={faTrash} style={{color: "#ffffff"}} /></button>
             <button className="btn-review btn">Review Signed   <FontAwesomeIcon icon={faArrowAltCircleRight} style={{color: "#ffffff"}} /></button>
